6c468a2a4e449e73d62612e122810fbe107df02e
Max Miliano <milianordelcanto@gmail.com>
2025-04-20 15:22:22 -0300

    refactor: scheme

--- scheme/scheme.go
+++ scheme/scheme.go
@@ -5,19 +5,19 @@ type Config struct {
 }
 
 type Highlights struct {
-	Editor  []interface{} `yaml:"editor"`
-	Syntax  []interface{} `yaml:"syntax"`
-	Plugins []interface{} `yaml:"plugins"`
+	Editor  []any `yaml:"editor"`
+	Syntax  []any `yaml:"syntax"`
+	Plugins []any `yaml:"plugins"`
 }
 
 type Scheme struct {
-	Name       string                 `yaml:"name"`
-	Version    string                 `yaml:"version"`
-	Author     string                 `yaml:"author"`
-	Repo       string                 `yaml:"repo"`
-	License    string                 `yaml:"license"`
-	Contact    string                 `yaml:"contact"`
-	Config     Config                 `yaml:"config"`
-	Palette    map[string]interface{} `yaml:"palette"`
-	Highlights Highlights             `yaml:"highlights"`
+	Name       string         `yaml:"name"`
+	Version    string         `yaml:"version"`
+	Author     string         `yaml:"author"`
+	Repo       string         `yaml:"repo"`
+	License    string         `yaml:"license"`
+	Contact    string         `yaml:"contact"`
+	Config     Config         `yaml:"config"`
+	Palette    map[string]any `yaml:"palette"`
+	Highlights Highlights     `yaml:"highlights"`
 }
